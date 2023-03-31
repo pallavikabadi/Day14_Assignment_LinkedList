5fc38bded513933edaa9dc911ee75f5dbee3627f
pallavi kabadi <pallavi08jadhav@gmail.com>
2023-03-31 13:00:31 +0530

    added:Delete First Element From Linked List

--- src/com/bridgelabz/LinkedList.java
+++ src/com/bridgelabz/LinkedList.java
@@ -46,4 +46,13 @@ public class LinkedList <T>{
         }
         return false;
     }
+
+    public T pop() {
+
+        if (head == null)
+            return null;
+        T popData = head.data;
+        head = head.next;
+        return popData;
+    }
 }
--- src/com/bridgelabz/LinkedListMain.java
+++ src/com/bridgelabz/LinkedListMain.java
@@ -2,17 +2,24 @@ package com.bridgelabz;
 
 public class LinkedListMain {
     public static void main(String[] args) {
-    LinkedList<Integer>linkedList1 = new LinkedList<>();
+
+        LinkedList<Integer>linkedList1 = new LinkedList<>();
         linkedList1.append(56);
         linkedList1.append(70);
         linkedList1.display();
 
-    Boolean isInserted = linkedList1.insertAfter(56,30);
+        Boolean isInserted = linkedList1.insertAfter(56,30);
         if (isInserted)
             System.out.println("Element Inserted Successfully.");
         else
-                System.out.println("Element is not Inserted!");
-
+            System.out.println("Element is not Inserted!");
         linkedList1.display();
-}
+
+        Integer poppedData = linkedList1.pop();
+        if (poppedData == null)
+            System.out.println("LinkedList is Empty");
+        else
+            System.out.println("The Element popped is "+poppedData);
+        linkedList1.display();
+    }
 }
