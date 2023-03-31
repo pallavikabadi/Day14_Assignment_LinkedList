ee452531bc8b2bda2cc7d3c50588d2a6f8741d98
pallavi kabadi <pallavi08jadhav@gmail.com>
2023-03-31 12:31:47 +0530

    added:Assignment 14 Linked List

---
+++ src/com/bridgelabz/LinkedList.java
@@ -0,0 +1,28 @@
+package com.bridgelabz;
+
+public class LinkedList <T>{
+    Node<T> head;
+    Node<T> tail;
+
+    public void add(T data) {
+
+        Node<T> newNode = new Node<>(data);
+        if (head == null) {
+            head = newNode;
+            tail = newNode;
+        }else {
+            tail.next = newNode;
+            tail = newNode;
+        }
+    }
+
+    public void display() {
+
+        Node<T> temp = head;
+        while (temp != null){
+            System.out.print(temp.data + " -> ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+}
---
+++ src/com/bridgelabz/LinkedListMain.java
@@ -0,0 +1,13 @@
+package com.bridgelabz;
+
+public class LinkedListMain {
+    public static void main(String[] args) {
+        LinkedList<Integer> linkedList1 = new LinkedList<>();
+        linkedList1.add(56);
+        linkedList1.add(30);
+        linkedList1.add(70);
+
+        linkedList1.display();
+
+    }
+}
